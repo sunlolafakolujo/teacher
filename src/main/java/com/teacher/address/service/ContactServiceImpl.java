82a0a7f74dee9bb9f6ea5287354fff6e3785847f
Sunlola Fakolujo <dapafol@live.com>
2022-08-03 00:58:04 +0100

    Contact service implementation

--- src/main/java/com/teacher/address/service/ContactServiceImpl.java
+++ src/main/java/com/teacher/address/service/ContactServiceImpl.java
@@ -2,13 +2,18 @@ package com.teacher.address.service;
 
 import com.teacher.address.dao.ContactRepository;
 import com.teacher.address.entity.Contact;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+import java.util.Optional;
+
 @Service
+@Slf4j
 public class ContactServiceImpl implements ContactService{
 
     @Autowired
@@ -35,16 +40,41 @@ public class ContactServiceImpl implements ContactService{
 
     @Override
     public Contact updateContact(Contact contact, Long id) {
-        return null;
+        Contact savedContact=contactRepository.findById(id).orElseThrow(()-> new RuntimeException("Contact "+id+" does not exist"));
+
+        if (Objects.nonNull(contact.getStreetNumber()) || "".equalsIgnoreCase(contact.getStreetNumber())){
+            savedContact.setStreetNumber(contact.getStreetNumber());
+        }if (Objects.nonNull(contact.getStreetName())||"".equalsIgnoreCase(contact.getStreetName())){
+            savedContact.setStreetName(contact.getStreetName());
+        }if (Objects.nonNull(contact.getCity())||"".equalsIgnoreCase(contact.getCity())){
+            savedContact.setCity(contact.getCity());
+        }if (Objects.nonNull(contact.getPostZipCode())||"".equalsIgnoreCase(contact.getPostZipCode())){
+            savedContact.setPostZipCode(contact.getPostZipCode());
+        }if (Objects.nonNull(contact.getLandMark())||"".equalsIgnoreCase(contact.getLandMark())){
+            savedContact.setLandMark(contact.getLandMark());
+        }if (Objects.nonNull(contact.getState_province()) || "".equalsIgnoreCase(contact.getState_province())){
+            savedContact.setState_province(contact.getState_province());
+        }if (Objects.nonNull(contact.getCountry())||"".equalsIgnoreCase(contact.getCountry())){
+            savedContact.setCountry(contact.getCountry());
+        }
+
+        return contactRepository.save(contact);
     }
 
     @Override
     public void deleteContactById(Long id) {
+        contactRepository.deleteById(id);
+
+        Optional<Contact> optionalContact=contactRepository.findById(id);
+
+        if (optionalContact.isEmpty()){
+            log.info("Contact is deleted");
+        }else throw new RuntimeException("Contact is not deleted");
 
     }
 
     @Override
     public void deleteAllContact() {
-
+        contactRepository.deleteAll();
     }
 }
