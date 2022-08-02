4aaf1d9411bd44174f1b2870870d72c89091c465
Sunlola Fakolujo <dapafol@live.com>
2022-08-03 00:59:06 +0100

    contact service test

--- src/test/java/com/teacher/address/service/ContactServiceImplTest.java
+++ src/test/java/com/teacher/address/service/ContactServiceImplTest.java
@@ -1,4 +1,37 @@
+package com.teacher.address.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ContactServiceImplTest {
-  
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void saveContact() {
+    }
+
+    @Test
+    void findContactById() {
+    }
+
+    @Test
+    void findAllContact() {
+    }
+
+    @Test
+    void updateContact() {
+    }
+
+    @Test
+    void deleteContactById() {
+    }
+
+    @Test
+    void deleteAllContact() {
+    }
 }
