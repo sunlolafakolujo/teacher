c1f423d82463a3a5c45db165bd9bfbd02e7c3812
sunlola <fakolujos@gmail.com>
2022-08-03 23:42:33 +0100

    Base audit class to track datecreated and datemodified

--- src/main/java/com/teacher/baseaudit/BaseAudit.java
+++ src/main/java/com/teacher/baseaudit/BaseAudit.java
@@ -1,2 +1,24 @@
-package com.teacher.baseaudit;public class BaseAudit {
+package com.teacher.baseaudit;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseAudit implements Serializable {
+
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    private LocalDateTime modifiesDate;
 }
