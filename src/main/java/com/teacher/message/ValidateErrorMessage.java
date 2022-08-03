2144dc54ab2fc2c5bff8af6e557f5d0a5362f37a
sunlola <fakolujos@gmail.com>
2022-08-03 23:43:24 +0100

    API error message class

--- src/main/java/com/teacher/message/ValidateErrorMessage.java
+++ src/main/java/com/teacher/message/ValidateErrorMessage.java
@@ -1,2 +1,16 @@
-package com.teacher.message;public class ValidateErrorMessage {
+package com.teacher.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ValidateErrorMessage {
+
+    private HttpStatus httpStatus;
+
+    private String message;
 }
