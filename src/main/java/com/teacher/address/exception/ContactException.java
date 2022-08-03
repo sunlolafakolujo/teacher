eaaa366cccb3ac43328cc2076edfc244efe82da8
sunlola <fakolujos@gmail.com>
2022-08-03 23:39:39 +0100

    contact exception created

--- src/main/java/com/teacher/address/exception/ContactException.java
+++ src/main/java/com/teacher/address/exception/ContactException.java
@@ -1,2 +1,23 @@
-package com.teacher.address.exception;public class ContactException {
+package com.teacher.address.exception;
+
+public class ContactException extends RuntimeException{
+    public ContactException() {
+        super();
+    }
+
+    public ContactException(String message) {
+        super(message);
+    }
+
+    public ContactException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ContactException(Throwable cause) {
+        super(cause);
+    }
+
+    protected ContactException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
