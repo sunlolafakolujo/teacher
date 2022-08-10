9dfec1b30904c1e9d7560f6619c061dc9418a202
sunlola <fakolujos@gmail.com>
2022-08-10 17:22:54 +0100

    New creation

--- src/main/java/com/teacher/staticdata/MeansOfIdentification.java
+++ src/main/java/com/teacher/staticdata/MeansOfIdentification.java
@@ -1,2 +1,24 @@
-package com.teacher.staticdata;public enum MeansOfIdentification {
+package com.teacher.staticdata;
+
+public enum MeansOfIdentification {
+    NIN("NIN"),
+    PASSPORT("PASSPORT"),
+    DRIVERS_LICENSE("DRIVERS_LICENSE");
+
+    private final String meansOfIdentification;
+
+    MeansOfIdentification(String meansOfIdentification) {
+        this.meansOfIdentification = meansOfIdentification;
+    }
+
+    public String getMeansOfIdentification() {
+        return meansOfIdentification;
+    }
+
+    @Override
+    public String toString() {
+        return "MeansOfIdentification{" +
+                "meansOfIdentification='" + meansOfIdentification + '\'' +
+                '}';
+    }
 }
