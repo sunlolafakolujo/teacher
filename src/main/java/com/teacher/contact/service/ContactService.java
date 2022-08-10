e9fb166e4f6df38be1514f7366bf82ac223ea35b
sunlola <fakolujos@gmail.com>
2022-08-10 17:19:19 +0100

    Added pagination to the findAllContact method

--- src/main/java/com/teacher/contact/service/ContactService.java
+++ src/main/java/com/teacher/contact/service/ContactService.java
@@ -2,8 +2,6 @@ package com.teacher.contact.service;
 
 import com.teacher.contact.model.Contact;
 import com.teacher.contact.exception.ContactException;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
