dcaced6ae254fef2826ef27ad81acedf19360c5a
sunlola <fakolujos@gmail.com>
2022-08-10 22:25:37 +0100

    No change

--- src/main/java/com/teacher/contact/model/Contact.java
+++ src/main/java/com/teacher/contact/model/Contact.java
@@ -1,12 +1,15 @@
 package com.teacher.contact.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.teacher.baseaudit.BaseAudit;
+import com.teacher.qualification.model.Qualification;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
 
 
 @Data
@@ -37,4 +40,8 @@ public class Contact extends BaseAudit {
 
     @NotEmpty(message = "Country can't be blank")
     private String country;
+
+//    @JsonIgnore
+//    @OneToOne(mappedBy = "contact", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+//    private Qualification qualification;
 }
