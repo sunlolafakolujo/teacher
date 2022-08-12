008f9f3b947a15534a5b3b54fc712110e326aa2d
sunlola <fakolujos@gmail.com>
2022-08-12 20:13:33 +0100

    Additional attributes added

--- src/main/java/com/teacher/message/ValidateErrorMessage.java
+++ src/main/java/com/teacher/message/ValidateErrorMessage.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import java.util.Date;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -13,4 +15,8 @@ public class ValidateErrorMessage {
     private HttpStatus httpStatus;
 
     private String message;
+
+    private String description;
+
+    private Date timestamp;
 }
