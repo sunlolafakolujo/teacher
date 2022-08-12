3aa7fbf91c520d7cb90e216f4b5912e3ddc9a61b
sunlola <fakolujos@gmail.com>
2022-08-12 20:14:11 +0100

    Refactored

--- src/main/java/com/teacher/qualification/exception/QualificationRestExceptionHandler.java
+++ src/main/java/com/teacher/qualification/exception/QualificationRestExceptionHandler.java
@@ -6,16 +6,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-@ResponseStatus
-@ControllerAdvice
+
+import java.util.Date;
+
+@RestControllerAdvice
 public class QualificationRestExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(QualificationNotFoundException.class)
-    public ResponseEntity<ValidateErrorMessage> qualificationExceptionHandler(QualificationNotFoundException qualificationException){
+    public ResponseEntity<ValidateErrorMessage> qualificationExceptionHandler(
+            QualificationNotFoundException qualificationException, WebRequest wr){
 
-        ValidateErrorMessage vem=new ValidateErrorMessage(HttpStatus.NOT_FOUND, qualificationException.getMessage());
+        ValidateErrorMessage vem=new ValidateErrorMessage(HttpStatus.NOT_FOUND,
+                qualificationException.getMessage(),
+                wr.getDescription(false),
+                new Date());
 
-        return new ResponseEntity<>(vem, HttpStatus.NOT_FOUND);
+        return ResponseEntity.internalServerError().body(vem);
     }
 }
