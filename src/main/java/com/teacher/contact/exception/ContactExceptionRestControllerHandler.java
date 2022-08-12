203781b4bfec7a35cb55208ad8b29e34264e88c5
sunlola <fakolujos@gmail.com>
2022-08-12 20:13:00 +0100

    Refactored

--- src/main/java/com/teacher/contact/exception/ContactExceptionRestControllerHandler.java
+++ src/main/java/com/teacher/contact/exception/ContactExceptionRestControllerHandler.java
@@ -6,18 +6,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
-@ResponseStatus
+import java.util.Date;
+
+@RestControllerAdvice
 public class ContactExceptionRestControllerHandler extends ResponseEntityExceptionHandler {
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ContactNotFoundException.class)
-    public ResponseEntity<ValidateErrorMessage> contactExceptionRestController(ContactNotFoundException contactException){
+    public ResponseEntity<ValidateErrorMessage> contactExceptionRestController(
+            WebRequest wr,ContactNotFoundException contactException){
 
-        ValidateErrorMessage vem=new ValidateErrorMessage(HttpStatus.NOT_FOUND, contactException.getMessage());
+        ValidateErrorMessage vem=new ValidateErrorMessage(HttpStatus.NOT_FOUND,
+                contactException.getMessage(),
+                wr.getDescription(false),
+                new Date());
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(vem);
+        return ResponseEntity.internalServerError().body(vem);
     }
 
 
