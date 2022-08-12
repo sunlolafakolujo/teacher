632c62ff62efef859f2700e25688c889ed1c219a
sunlola <fakolujos@gmail.com>
2022-08-12 20:14:52 +0100

    Refactored

--- src/main/java/com/teacher/workexperience/exception/WorkExperienceRestExceptionHandler.java
+++ src/main/java/com/teacher/workexperience/exception/WorkExperienceRestExceptionHandler.java
@@ -6,17 +6,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
-@ResponseStatus
+import java.util.Date;
+
+@RestControllerAdvice
 public class WorkExperienceRestExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(WorkExperienceNotFoundException.class)
-    public ResponseEntity<ValidateErrorMessage> workExperienceNotFoundException(WorkExperienceNotFoundException workExperienceNotFoundException){
+    public ResponseEntity<ValidateErrorMessage> workExperienceNotFoundException(
+            WorkExperienceNotFoundException workExperienceNotFoundException, WebRequest wr){
 
-        ValidateErrorMessage vem=new ValidateErrorMessage(HttpStatus.NOT_FOUND, workExperienceNotFoundException.getMessage());
+        ValidateErrorMessage vem=new ValidateErrorMessage(HttpStatus.NOT_FOUND,
+                workExperienceNotFoundException.getMessage(),
+                wr.getDescription(false),
+                new Date());
 
-        return new ResponseEntity<>(vem, HttpStatus.NOT_FOUND);
+        return ResponseEntity.internalServerError().body(vem);
     }
 }
